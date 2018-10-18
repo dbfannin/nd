a7c0a758b04218cf79fcc95d35a362a12eacda61
David <davidf@realtracs.net>
2018-10-18 07:18:09 -0500

    add link text below profile images

--- src/profiles.data.ts
+++ src/profiles.data.ts
@@ -3,8 +3,8 @@ import {GamesEnum} from './app/profile/games.enum';
 
 export const DangerDude: ProfileInterface = {
   twitchUserName: 'dangerdrake89',
-  // twitchUrl: 'https://www.twitch.tv/dangerdrake89',
-  mixerUrl: 'https://mixer.com/DangerDude',
+  // twitchUrl: 'twitch.tv/dangerdrake89',
+  mixerUrl: 'mixer.com/DangerDude',
   imageUrl: '/assets/dangerdrake.png',
   displayName: 'DangerDude',
   games: [GamesEnum.SevenDaysToDie, GamesEnum.Scum],
@@ -13,8 +13,8 @@ export const DangerDude: ProfileInterface = {
 
 export const TurboTron: ProfileInterface = {
   twitchUserName: 'turbo_tron',
-  // twitchUrl: 'https://www.twitch.tv/turbo_tron',
-  mixerUrl: 'https://mixer.com/Turbo_Tron',
+  // twitchUrl: 'twitch.tv/turbo_tron',
+  mixerUrl: 'mixer.com/Turbo_Tron',
   imageUrl: '/assets/turbo_tron.png',
   displayName: 'Turbo_Tron',
   games: [GamesEnum.SevenDaysToDie, GamesEnum.Scum],
@@ -23,8 +23,8 @@ export const TurboTron: ProfileInterface = {
 
 export const Sol: ProfileInterface = {
   twitchUserName: 'nd_sol',
-  twitchUrl: 'https://www.twitch.tv/nd_sol',
-  // mixerUrl: 'https://mixer.com/ND_Sol',
+  twitchUrl: 'twitch.tv/nd_sol',
+  // mixerUrl: 'mixer.com/ND_Sol',
   imageUrl: '/assets/sol.png',
   displayName: 'Sol',
   games: [GamesEnum.Fortnite, GamesEnum.Scum],
